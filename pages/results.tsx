a76075c009667ac0917c95daef5648597981273d
= <=>
2022-11-07 15:52:21 -0600

    chore: fix errors

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,11 +1,8 @@
-import { useRouter } from "next/router"
 import PrimaryLayout from "../components/layouts/primary/PrimaryLayout"
 import SearchResult from "../components/SearchResult/SearchResult"
 import { NextPageWithLayout } from "./page"
 
 const Results: NextPageWithLayout = () => {
-  const { locale } = useRouter()
-
   return (
     <section className="flex flex-col items-center gap-y-5">
       <div className="flex flex-col space-y-8">
